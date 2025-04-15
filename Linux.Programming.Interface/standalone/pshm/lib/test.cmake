fba696f34d16604419287958f8d7f41164be00a3
hexu <hexu_1985@sina.com>
2025-04-15 11:31:41 +0800

    update Linux.Programming.Interface/standalone/pshm/lib

--- Linux.Programming.Interface/standalone/pshm/lib/test.cmake
+++
@@ -1,7 +0,0 @@
-
-add_custom_command(
-    OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/ename.c.inc
-    COMMAND sh ${CMAKE_CURRENT_SOURCE_DIR}/Build_ename.sh 
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMENT "Generating ename.c.inc"
-)
